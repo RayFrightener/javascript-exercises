f71a393c6c9b7c76bce187dbf154fc203cc22b3f
Pranshu <pranshuchawla19@gmail.com>
2023-08-08 14:07:14 -0400

    exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const charArray = str.split("");
+    const reversedArray = charArray.reverse();
+    const reversedString = reversedArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
