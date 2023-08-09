5e0a310db283dba55c5dbd8d28fc34bcd0a8e5e9
Pranshu <pranshuchawla19@gmail.com>
2023-08-09 11:43:16 -0400

    exercise 6 done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 !== 0) {
+        return false;
+    }
+    
+    // Years divisible by 100 are not leap years unless they are also divisible by 400
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false;
+    }
+    
+    // If the year is divisible by 4 and not divisible by 100 (or divisible by 400), it's a leap year
+    return true;
 };
 
 // Do not edit below this line
