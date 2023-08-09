750f139bb58930f95d897466e7d4744fe1509bdc
Pranshu <pranshuchawla19@gmail.com>
2023-08-08 21:01:31 -0400

    exercise 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let c =0;
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+    if(start < 0){
+        return "ERROR";
+    }
+    else if(typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
+    for(let i=start;i<=end;i++){
+        c+=i
+    } 
+    return c; 
 };
 
 // Do not edit below this line
