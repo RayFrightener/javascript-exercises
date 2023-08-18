cf0aca8be0bea935532f3827d838afceeca94191
Pranshu <pranshuchawla19@gmail.com>
2023-08-18 12:12:07 -0400

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+  
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a-b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = 0;
+  for (let i =0; i < arr.length;i++){
+    sum += arr[i];
+  }
+  return sum;
+  
 	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multiplication = 1; // Initialize the multiplication result to 1
+  
+  for (let i = 0; i < arr.length; i++) {
+    multiplication *= arr[i];
+  }
+  
+  return multiplication; // Return the final multiplication result
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1; // Initialize the result to 1
+  
+  for (let i = 0; i < b; i++) {
+    result *= a; // Multiply the result by 'a' in each iteration
+  }
+  
+  return result;
 };
 
 const factorial = function() {
