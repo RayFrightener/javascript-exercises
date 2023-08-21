77c70a371bd3f42533cf320cd3ff0940fd53cae0
Pranshu <pranshuchawla19@gmail.com>
2023-08-21 00:40:37 -0400

    palindrom done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+word = word.toLowerCase();
+word = word.replace(/[^a-z0-9]/g, '');
+const reversedWord = word.split("").reverse().join("");
+return word ===  reversedWord;
 };
 
 // Do not edit below this line
