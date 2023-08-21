8027dbe083d32a229332832eda8121dc3fb111b4
Pranshu <pranshuchawla19@gmail.com>
2023-08-21 16:44:29 -0400

    fibonacci fnc created

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n <= 0) {
+        return 0;
+      } else if (n === 1) {
+        return 1;
+      } else {
+        let prev = 0;
+        let curr = 1;
+        for (let i = 2; i <= n; i++) {
+          const next = prev + curr;
+          prev = curr;
+          curr = next;
+        }
+        return curr;
+      }
 };
 
 // Do not edit below this line
